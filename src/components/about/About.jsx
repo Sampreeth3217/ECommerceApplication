f1d41a951250568c5d37261e7fceac51ebfe918c
Sampreeth3217 <127882606+Sampreeth3217@users.noreply.github.com>
2024-06-28 22:57:10 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import './About.css'
+import "./About.css";
+import { ImArrowDown2 } from "react-icons/im";
+
 function About() {
   return (
     <div>
-      About
+      <h2>
+        About Us <ImArrowDown2 />
+      </h2>
+      <p className="mt-5 container text-center">
+        John Wick, a retired but deadly hitman, lives a peaceful life with his beloved wife, Helen. However, his world is shattered when Helen succumbs to a terminal illness. Before her death, she arranges for John to receive a beagle puppy named Daisy, giving him a chance to cope with his grief. John's life is further devastated when Iosef Tarasov, the son of a Russian crime lord, breaks into his home, steals his car, and kills Daisy.
+        Driven by vengeance, John embarks on a relentless quest to hunt down Iosef and his associates. His journey takes him back into the dark underworld he once left behind, where he encounters old friends and enemies. Known as "The Boogeyman," John Wick is feared and respected by all who know him. His lethal skills and determination lead to a series of brutal confrontations as he exacts his revenge.
+        Throughout his journey, John navigates the intricate web of alliances and rivalries within the criminal underworld. From confronting Viggo Tarasov, Iosef's father, to battling skilled assassins, John faces numerous challenges. His reputation as an unstoppable force grows with each encounter, cementing his status as a legendary assassin.
+        As John continues his vendetta, he uncovers deeper conspiracies and faces even greater threats. The bonds of loyalty, honor, and betrayal shape his path as he strives to survive in a world where trust is scarce, and danger lurks around every corner. Ultimately, John Wick's journey is one of relentless pursuit, driven by his unyielding desire for justice and retribution.
+      </p>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
